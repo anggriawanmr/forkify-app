f2b806eb9736bb984d28753711ef59ea4448f560
anggriawanmr <anggriawanmr@gmail.com>
2021-11-02 13:47:22 +0700

    Adding helpers.js :sparkles:

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/get';
+export const TIMEOUT_SEC = 10;
--- src/js/model.js
+++ src/js/model.js
@@ -1,17 +1,16 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
+    const data = await getJSON(`${API_URL}/${id}`);
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    let { recipe } = data.data;
+    const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
       title: recipe.title,
@@ -24,6 +23,6 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(err);
   }
 };
